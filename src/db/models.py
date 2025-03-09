44c7e85ce5504ebd0b802a81d881b1ebe117b884
zhandos256 <r.zhandos256@gmail.com>
2025-03-09 13:35:33 +0500

    update models

--- src/db/models.py
+++ src/db/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
 from core.const import TIMEZONE
 
+
 class Base(DeclarativeBase):
     pass
 
@@ -13,11 +14,11 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    created: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
+    created: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now())
     tg_userid: Mapped[int] = mapped_column(BigInteger, unique=True, index=True)
     username: Mapped[str] = mapped_column(String(length=32), default="-", nullable=True)
     first_name: Mapped[str] = mapped_column(String(length=255), default="-", nullable=True)
     last_name: Mapped[str] = mapped_column(String(length=255), default="-", nullable=True)
     language: Mapped[str] = mapped_column(String(length=2), default="ru", nullable=True)
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
-    last_update: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(TIMEZONE))
+    last_action: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(TIMEZONE))
