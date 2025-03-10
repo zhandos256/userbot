00e9ff02284e6097c84f8a720db654294e03cbf4
zhandos256 <r.zhandos256@gmail.com>
2025-03-10 11:35:26 +0500

    refactor

--- src/middleware/i18n_middleware.py
+++ src/middleware/i18n_middleware.py
@@ -3,16 +3,16 @@ from typing import Any, Dict
 from aiogram.types import TelegramObject
 from aiogram.utils.i18n import I18n, I18nMiddleware
 
-from core.const import LOCALES_DIR, DEFAULT_LOCALE, DOMAIN_MESSAGES
-from db.query import get_user_lang
+from config.const import LOCALES_DIR, DEFAULT_LOCALE, DOMAIN_MESSAGES
+from database.query import get_user_repository
 
 
 class CustomI18nMiddleware(I18nMiddleware):
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
         user = data["event_from_user"]
         userid = int(user.id)
-        res = await get_user_lang(tg_userid=userid)
-        return res if res else DEFAULT_LOCALE
+        user_repo = await get_user_repository()
+        return await user_repo.get_user_lang(tg_userid=userid) or DEFAULT_LOCALE
 
 
 i18n = I18n(path=LOCALES_DIR, default_locale=DEFAULT_LOCALE, domain=DOMAIN_MESSAGES)
--- src/middleware/last_action_middleware.py
+++ src/middleware/last_action_middleware.py
@@ -1,7 +1,7 @@
 from aiogram import BaseMiddleware
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
-from db.query import update_last_action
+from database.query import get_user_repository
 
 
 class LastActionMiddleware(BaseMiddleware):
@@ -13,5 +13,6 @@ class LastActionMiddleware(BaseMiddleware):
         # return await super().__call__(handler, event, data)
         user = data["event_from_user"]
         user_id = int(user.id)
-        await update_last_action(tg_userid=user_id)
+        user_repo = await get_user_repository()
+        await user_repo.update_last_action(tg_userid=user_id)
         return await handler(event, data)
