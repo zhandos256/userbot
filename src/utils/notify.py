ebc283e3a798e2fa283cf66b630e0a7858c0d69f
zhandos256 <r.zhandos256@gmail.com>
2025-03-13 13:54:04 +0500

    refactor

--- src/utils/notify.py
+++ src/utils/notify.py
@@ -3,30 +3,22 @@ import logging
 
 from aiogram import Bot
 
-from database.query import get_user_repository
+from database.query import get_all_users
 
+
+async def notify_users_by_condition(bot: Bot, text: str, is_admin: bool):
+    users = await get_all_users()
+    if users is not None:
+        for user in users:
+            if user.is_admin == is_admin:
+                try:
+                    await bot.send_message(chat_id=user.tg_userid, text=text)
+                    await asyncio.sleep(0.05)
+                except Exception as e:
+                    logging.exception(e)
 
 async def notify_admins(bot: Bot, text: str):
-    user_repo = await get_user_repository()
-    users = await user_repo.get_all_users()
-    if users is not None:
-        for user in users:
-            if user.is_admin:
-                try:
-                    await bot.send_message(chat_id=user.tg_userid, text=text)
-                    await asyncio.sleep(0.05)
-                except Exception as e:
-                    logging.exception(e)
-
+    await notify_users_by_condition(bot, text, is_admin=True)
 
 async def notify_users(bot: Bot, text: str):
-    user_repo = await get_user_repository()
-    users = await user_repo.get_all_users()
-    if users is not None:
-        for user in users:
-            if not user.is_admin:
-                try:
-                    await bot.send_message(chat_id=user.tg_userid, text=text)
-                    await asyncio.sleep(0.05)
-                except Exception as e:
-                    logging.exception(e)
+    await notify_users_by_condition(bot, text, is_admin=False)
