fc932dbdddaa1ca658d4c8c2ae490f0e419c6efd
zhandos256 <r.zhandos256@gmail.com>
2025-03-16 10:44:05 +0500

    refactor

--- src/handlers/users/cancel.py
+++ src/handlers/users/cancel.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from aiogram import F, Router, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
@@ -7,21 +9,44 @@ from keyboards.inline.menu import back_menu_kb
 router = Router()
 
 
+async def msg_with_state(upd: types.Message) -> None:
+    await upd.answer(text="Операция отменена!", reply_markup=back_menu_kb())
+
+
+async def msg_without_state(upd: types.Message) -> None:
+    await upd.answer(text="Нечего отменять!", reply_markup=back_menu_kb())
+
+
+async def callback_with_state(upd: types.CallbackQuery) -> None:
+    await upd.message.edit_text(text="Операция отменена!", reply_markup=back_menu_kb())
+
+
+async def callback_without_state(upd: types.CallbackQuery) -> None:
+    await upd.answer(text="Нечего отменять!", show_alert=True)
+
+
 @router.message(Command("cancel"))
-async def cancel_msg(msg: types.Message, state: FSMContext):
-    st = await state.get_state()
-    if st is not None:
-        await msg.answer(text="Операция отменена!", reply_markup=back_menu_kb())
-        await state.clear()
-    else:
-        await msg.answer(text="Нечего отменять!", reply_markup=back_menu_kb())
-
-
 @router.callback_query(F.data == "cancel")
-async def cancel_cb(call: types.CallbackQuery, state: FSMContext):
+async def cancel_msg(update: Union[types.Message, types.CallbackQuery], state: FSMContext) -> None:
     st = await state.get_state()
+
+    handlers = {
+        types.Message: {
+            "with_state": msg_with_state,
+            "without_state": msg_without_state,
+        },
+        types.CallbackQuery: {
+            "with_state": callback_with_state,
+            "without_state": callback_without_state,
+        }
+    }
+
+    handler = handlers.get(type(update))
+    if handler is None:
+        return
+
     if st is not None:
-        await call.message.edit_text(text="Операция отменена!", reply_markup=back_menu_kb())
+        await handler["with_state"](update)
         await state.clear()
     else:
-        await call.answer(text="Нечего отменять!", show_alert=True)
+        await handler["without_state"](update)
