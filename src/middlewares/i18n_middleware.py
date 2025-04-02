bd6a1634bf13174997b5a3736a675e4d7bdb6764
zhandos256 <r.zhandos256@gmail.com>
2025-04-02 13:23:07 +0500

    refactor

--- src/middlewares/i18n_middleware.py
+++ src/middlewares/i18n_middleware.py
@@ -3,15 +3,15 @@ from typing import Any, Dict
 from aiogram.types import TelegramObject, User as AiogramUser
 from aiogram.utils.i18n import I18n, I18nMiddleware
 
-from config.user_settings import settings
+from config.user_settings import settings, LOCALES_DIR
 from database.user_queries import get_user_lang
 
 
 class CustomI18nMiddleware(I18nMiddleware):
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
         user: AiogramUser = data["event_from_user"]
-        return await get_user_lang(tg_userid=user.id) or settings.DEFAULT_LOCALE
+        return await get_user_lang(tg_userid=user.id) or settings.default_locale
 
 
-i18n = I18n(path=settings.LOCALES_DIR, default_locale=settings.DEFAULT_LOCALE, domain=settings.DOMAIN_MESSAGES)
+i18n = I18n(path=LOCALES_DIR, default_locale=settings.default_locale, domain=settings.default_locale)
 i18n_middleware = CustomI18nMiddleware(i18n=i18n)
